7975cd1b88152353585dd52a1f3bdbd6b5fec037
Brian Besaw <bjbesaw@outlook.com>
2017-11-13 12:16:37 -0500

    set and findslot added, findslot incrementer fixed

--- hashmap.js
+++ hashmap.js
@@ -1,1 +1,84 @@
 'use strict';
+
+class HashMap {
+  constructor(initialCapacity=8) {
+    this.length = 0;
+    this._slots = [];
+    this._capacity = initialCapacity;
+  }
+
+  static _hashString(string) {
+    let hash = 5381;
+    for (let i=0; i<string.length; i++) {
+      hash = (hash << 5) + hash + string.charCodeAt(i);
+      hash = hash & hash;
+    }
+    return hash >>> 0;
+  }
+
+  set(key, value) {
+    const loadRatio = (this.length + 1) / this._capacity;
+    if (loadRatio > HashMap.MAX_LOAD_RATIO) {
+      this._resize(this._capacity * HashMap.SIZE_RATIO);
+    }
+
+    const index = this._findSlot(key);
+    //console.log('index ', index);
+    if(!this._slots[index]) {
+      this.length++; 
+    }
+    this._slots[index] = {
+      key,
+      value
+    };
+
+  }
+
+  _findSlot(key) {
+    const hash = HashMap._hashString(key);
+    const start = hash % this._capacity;
+
+    for (let i=start; i<start + this._capacity; i++) {
+      const index = i % this._capacity;
+      const slot = this._slots[index];
+      if (slot === undefined || slot.key === key) {     // second eq "=="?
+        return index;
+      }
+    }
+  }
+
+  _resize(size) {
+    const oldSlots = this._slots;
+    this._capacity = size;
+    // Reset the length - it will get rebuilt as you add the items back
+    this.length = 0;
+    this._slots = [];
+
+    for (const slot of oldSlots) {
+      if (slot !== undefined) {
+        this.set(slot.key, slot.value);
+      }
+    }
+  }
+
+
+
+  
+}
+
+HashMap.MAX_LOAD_RATIO = 0.9;
+HashMap.SIZE_RATIO = 3;
+
+// const myHashMap = new HashMap();
+// console.log(myHashMap.length);
+// myHashMap.set("wendy", "november");
+// console.log('---- first set ----');
+// console.log(myHashMap);
+// console.log(myHashMap.length);
+// myHashMap.set("wendy", "december");
+// console.log(myHashMap);
+// myHashMap.set("wendy", "january");
+// console.log(myHashMap);
+
+// myHashMap.set("giri", "may");
+// console.log(myHashMap);
